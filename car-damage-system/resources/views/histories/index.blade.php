7400fcb93a75affeadee1af0f26dd1921427a134
baguadam <baguadam@gmail.com>
2023-11-17 20:36:28 +0100

    User can research from history

--- car-damage-system/resources/views/histories/index.blade.php
+++ car-damage-system/resources/views/histories/index.blade.php
@@ -2,12 +2,12 @@
     <x-slot name="title">History</x-slot>
     <h1 class="text-4xl mb-4 font-extrabold mr-5">Search histories</h1>
     <h2 class="mb-6 font-normal">You can see your search histories below. Only searches with valid license plate format
-        are listed here. In case of license plates that don't exist in the database, a default pictore is displayed.
+        are listed here. In case of license plates that don't exist in the database, a default pictore is displayed. By clicking on a history,
+        you can perform the search again.
     </h2>
     <div class="flex flex-wrap justify-center">
     @foreach ($histories as $history)
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow m-4">
-
+        <a href=" {{ route('damages.search', ['license_plate' => $history->license]) }} " class="max-w-sm bg-white border border-gray-200 rounded-lg shadow m-4">
             <img class="max-w-[200px]" src="{{ asset('storage/images/' . $history->image_hash_name) }}" alt="Picture of the related car" />
 
             <dl class="max-w-md text-gray-900 divide-y divide-gray-200 p-3">
@@ -21,7 +21,7 @@
                     <dd class="text-md font-semibold">{{ $history->search_time }}</dd>
                 </div>
             </dl>
-        </div>
+        </a>
     @endforeach
     </div>
     <div class="mx-auto p-5 w-4/5">
